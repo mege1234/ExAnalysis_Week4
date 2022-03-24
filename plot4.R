db5a905a0df8cf0ae67bd277dc7261ed7d01df21
Megan Ruda <93596092+mege1234@users.noreply.github.com>
2022-03-24 16:35:24 -0500

    Add files via upload

---
+++ plot4.R
@@ -0,0 +1,19 @@
+#install packages
+library(dplyr)
+library(ggplot2)
+
+#read files
+NEI <- readRDS("summarySCC_PM25.rds")
+SCC <- readRDS("Source_Classification_Code.rds")
+
+#subset data
+NEIsub <- subset(NEI, fips == "24510")
+
+NEIEmissions <- NEIsub %>%
+    group_by(year, type) %>%
+    summarise(Emissions = sum(Emissions))
+head(NEIEmissions)
+#create plot
+png(filename = "./plot3.png", width =480, height = 480)
+ggplot(NEIEmissions, aes(year, Emissions, color = type)) + geom_point()
+dev.off()
