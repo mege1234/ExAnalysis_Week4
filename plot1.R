f04b247fa79066efd0684925440b67e3a5be9737
Megan Ruda <93596092+mege1234@users.noreply.github.com>
2022-03-24 16:09:37 -0500

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -1,19 +1,18 @@
+#install packages
 library(dplyr)
+
+#read files
 NEI <- readRDS("summarySCC_PM25.rds")
 SCC <- readRDS("Source_Classification_Code.rds")
-#head(NEI)
-#head(SCC)
-#summary(NEI)
-#dim(NEI)
-#str(NEI)
-#summary(NEI$Emissions)
+
+#subset data
 NEIsub <- subset(NEI, year = c(1999, 2002, 2005, 2008))
-#dim(NEIsub)
-#str(NEIsub)
-#summary(NEIsub)
 NEIyear <- NEIsub$year
 NEIEmissions <- NEIsub %>%
     group_by(year) %>%
     summarise(Emissions = sum(Emissions))
+
+#create plot
 png(filename = "./plot1.png", width =480, height = 480)
 plot(NEIEmissions$year, NEIEmissions$Emissions, main = "Total Emissions by Year", xlab = "year", ylab = "total emissions", pch = 19)
+dev.off()
